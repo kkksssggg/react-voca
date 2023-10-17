1343e9beba8c3c9314563b41a15350f640dd9cec
kimseulgi <ksk177985@gmail.com>
2023-10-17 22:47:37 +0900

    last update

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
-import DayList from "./component/DayList";
+import DayList from "./component/DayList.tsx";
 import Header from "./component/Header";
-import Day from "./component/Day";
+import Day from "./component/Day.tsx";
 import {BrowserRouter, Route, Routes} from 'react-router-dom';
 import EmptyPage from "./component/EmptyPage";
+import CreateWord from "./component/CreateWord.tsx";
+import CreateDay from "./component/CreateDay";
 
 function App() {
   return (
@@ -12,6 +14,8 @@ function App() {
         <Routes>
           <Route path="/" element={<DayList/>}/>
           <Route path="/day/:day" element={<Day/>}/>
+          <Route path="/create_word" element={<CreateWord/>}/>
+          <Route path="/create_day" element={<CreateDay/>}/>
           <Route path="*" element={<EmptyPage/>}/>
 \        </Routes>
       </div>
